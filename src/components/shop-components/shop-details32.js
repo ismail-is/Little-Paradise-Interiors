971f050bec279121429ac2ae7bb02ef7688aafcb
ismail-is <ismailthalithanooji@gmail.com>
2025-01-18 15:11:01 +0530

    day1

--- src/components/shop-components/shop-details32.js
+++ src/components/shop-components/shop-details32.js
@@ -143,172 +143,67 @@ class ShopDetails32 extends Component {
 															</div>
 															</div>
 
-															
-															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
-															<div className="ltn__gallery-item-inner">
-																<div className="ltn__gallery-item-img">
-																<Link to="/LEGACY"> 
-																<img src={publicUrl+"assets/img/Allimgs/Picture117.webp"} alt="Image" />
-																	
-																	</Link>
-																</div>
-																<div className="ltn__gallery-item-info" >
-																	
-																<h4 className="go-top" style={{marginTop:'-170px'}}><Link to="/LEGACY">Legacy</Link></h4>
-																
-															
-																</div>
-															</div>
-															</div>
-
-
-															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
-															<div className="ltn__gallery-item-inner">
-																<div className="ltn__gallery-item-img">
-																<Link to="/Terra"> 
-																<img src={publicUrl+"assets/img/Allimgs/Picture65.webp"} alt="Image" />
-																	
-																	</Link>
-																</div>
-																<div className="ltn__gallery-item-info" >
-																	
-																<h4 className="go-top" style={{marginTop:'-170px'}}><Link to="/Terra">Terra</Link></h4>
-																
-															
-																</div>
-															</div>
-															</div>
-
-
-															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
-															<div className="ltn__gallery-item-inner">
-																<div className="ltn__gallery-item-img">
-																<Link to="/GamingRoom"> 
-																<img src={publicUrl+"assets/img/Allimgs/Picture34.webp"} alt="Image" />
-																	
-																	</Link>
-																</div>
-																<div className="ltn__gallery-item-info" >
-																	
-																<h4 className="go-top" style={{marginTop:'-170px'}}><Link to="/GamingRoom">GamingRoom</Link></h4>
-																
-															
-																</div>
-															</div>
-															</div>
-
-
-															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
-															<div className="ltn__gallery-item-inner">
-																<div className="ltn__gallery-item-img">
-																<Link to="/InGreen"> 
-																<img src={publicUrl+"assets/img/Allimgs/Picture75.webp"} alt="Image" />
-																	
-																	</Link>
-																</div>
-																<div className="ltn__gallery-item-info" >
-																	
-																<h4 className="go-top" style={{marginTop:'-170px'}}><Link to="/InGreen">In to the Green</Link></h4>
-																
-															
-																</div>
-															</div>
-															</div>
-
-
-															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
-															<div className="ltn__gallery-item-inner">
-																<div className="ltn__gallery-item-img">
-																<Link to="/SpecksDesk"> 
-																<img src={publicUrl+"assets/img/Allimgs/Picture91.webp"} alt="Image" />
-																	
-																	</Link>
-																</div>
-																<div className="ltn__gallery-item-info" >
-																	
-																<h4 className="go-top" style={{marginTop:'-150px'}}><Link to="/SpecksDesk">SpecksDesk</Link></h4>
-																
-															
-																</div>
-															</div>
-															</div>
-
-
-															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
-															<div className="ltn__gallery-item-inner">
-																<div className="ltn__gallery-item-img">
-																<Link to="/Dreamy"> 
-																<img src={publicUrl+"assets/img/Allimgs/Picture87.webp"} alt="Image" />
-																	
-																	</Link>
-																</div>
-																<div className="ltn__gallery-item-info" >
-																	
-																<h4 className="go-top" style={{marginTop:'-170px'}}><Link to="/Dreamy">Dreamy</Link></h4>
-																
-															
-																</div>
-															</div>
-															</div>
-
-
-
-															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
-															<div className="ltn__gallery-item-inner">
-																<div className="ltn__gallery-item-img">
-																<Link to="/Raw"> 
-																<img src={publicUrl+"assets/img/Allimgs/Picture83.webp"} alt="Image" />
-																	
-																	</Link>
-																</div>
-																<div className="ltn__gallery-item-info" >
-																	
-																<h4 className="go-top" style={{marginTop:'-170px'}}><Link to="/Raw">Raw</Link></h4>
-																
-															
-																</div>
-															</div>
-															</div>
-
-
-															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
-															<div className="ltn__gallery-item-inner">
-																<div className="ltn__gallery-item-img">
-																<Link to="/PERLIYAS"> 
-																<img src={publicUrl+"assets/img/Allimgs/Picture111.webp"} alt="Image" />
-																	
-																	</Link>
-																</div>
-																<div className="ltn__gallery-item-info" >
-																	
-																<h4 className="go-top" ><Link to="/PERLIYAS">Hospital - Dialysis Center</Link></h4>
-																
-															
-																</div>
-															</div>
-															</div>
-
-
-															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
-															<div className="ltn__gallery-item-inner">
-																<div className="ltn__gallery-item-img">
-																<Link to="/BACKtoBALANCE"> 
-																<img src={publicUrl+"assets/img/Allimgs/Picture123.webp	"} alt="Image" />
-																	
-																	</Link>
-																</div>
-																<div className="ltn__gallery-item-info" >
-																	
-																<h4 className="go-top" style={{marginTop:'-170px'}}><Link to="/BACKtoBALANCE">Back TO Balance </Link></h4>
-																
-															
-																</div>
-															</div>
-															</div>
-															{/* gallery-item */}
-														         
 														</div>
 
+
+														<div className="ltn__gallery-active row ltn__gallery-style-2 ltn__gallery-info-hide---">
+															<div className="ltn__gallery-sizer col-lg-12" />
+															{/* gallery-item */}
+															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
+															<div className="ltn__gallery-item-inner">
+																<div className="ltn__gallery-item-img">
+																<Link to="/TWL"> 
+																<img src={publicUrl+"assets/img/Allimgs/Picture95.webp"} alt="Image" />
+																	
+																	</Link>
+																</div>
+																<div className="ltn__gallery-item-info" >
+																	
+																<h4 className="go-top" style={{marginTop:'-170px'}}><Link to="/TWL">TWL</Link></h4>
+																
+															
+																</div>
+															</div>
+															</div>
+															
+
+
+															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
+															<div className="ltn__gallery-item-inner">
+																<div className="ltn__gallery-item-img">
+																<Link to="/Earthessence"> 
+																<img src={publicUrl+"assets/img/Allimgs/Picture45.webp"} alt="Image" />
+																	
+																	</Link>
+																</div>
+																<div className="ltn__gallery-item-info" >
+																	
+																<h4 className="go-top" ><Link to="/Earthessence">Earthessence</Link></h4>
+																
+															
+																</div>
+															</div>
+															</div>
+															
+
+															<div className="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12">
+															<div className="ltn__gallery-item-inner">
+																<div className="ltn__gallery-item-img">
+																<Link to="/Sky"> 
+																<img src={publicUrl+"assets/img/Allimgs/Picture57.webp"}  alt="Image" />
+																	
+																	</Link>
+																</div>
+																<div className="ltn__gallery-item-info" >
+																	
+																<h4 className="go-top" style={{marginTop:'-170px'}}><Link to="/Sky">Sky</Link></h4>
+																
+															
+																</div>
+															</div>
+															</div>
+
+														</div>
 
 
 
